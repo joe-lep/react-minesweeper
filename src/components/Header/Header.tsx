b69833fef019f0c877c71a46dee2d4aac2ff5aa1
Joseph Lepley <josephflepley+pro@gmail.com>
2024-11-16 15:35:24 -0500

    Mobile Layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,10 @@ import './Header.css';
 export default function Header() {
   return (
     <div className="header-container">
-      <header><h1>React Minesweeper</h1></header>
+      <header>
+        <h1 className="header-regular">React Minesweeper</h1>
+        <h1 className="header-small">RM</h1>
+      </header>
     </div>
   );
 }
