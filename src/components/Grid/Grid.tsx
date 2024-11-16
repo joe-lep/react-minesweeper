2e2a31f80e68643d84638b73296013e1a408e871
Joseph Lepley <josephflepley+pro@gmail.com>
2024-11-16 13:58:46 -0500

    Implement react-zoom-pan-pinch

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -2,6 +2,7 @@ import { useMemo } from "react";
 import Row from "./Row";
 import './Grid.css';
 import { useGameConfig } from "../GameManager";
+import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
 
 export default function Grid() {
   const { width, height } = useGameConfig();
@@ -14,10 +15,16 @@ export default function Grid() {
   );
 
   return (
-    <div className="grid-container-outer">
-      <div className="grid-container">
-        <div className="grid">{renderedRows}</div>
-      </div>
+    <div className="grid-container">
+        <TransformWrapper centerOnInit minScale={0.25}>
+          <div className="grid-container-inner">
+            <TransformComponent wrapperClass="grid-transform-wrapper">
+              <div className="grid">
+                {renderedRows}
+              </div>
+            </TransformComponent>
+          </div>
+        </TransformWrapper>
     </div>
   );
 }
