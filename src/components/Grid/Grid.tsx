68ab07b1a22cc1e07aa9945e77aededa614244a6
Joseph Lepley <josephflepley+pro@gmail.com>
2024-11-16 13:10:52 -0500

    Remove unused reffs from Grid

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,11 +1,9 @@
-import { useMemo, useRef } from "react";
+import { useMemo } from "react";
 import Row from "./Row";
 import './Grid.css';
 import { useGameConfig } from "../GameManager";
 
 export default function Grid() {
-  const gridContainerRef = useRef<HTMLDivElement>(null);
-  const gridRef = useRef<HTMLDivElement>(null);
   const { width, height } = useGameConfig();
 
   const renderedRows = useMemo(
@@ -17,8 +15,8 @@ export default function Grid() {
 
   return (
     <div className="grid-container-outer">
-      <div className="grid-container" ref={gridContainerRef}>
-        <div className="grid" ref={gridRef}>{renderedRows}</div>
+      <div className="grid-container">
+        <div className="grid">{renderedRows}</div>
       </div>
     </div>
   );
