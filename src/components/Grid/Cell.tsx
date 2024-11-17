58a9d0716372f763c12120c4edf25d623bf61f13
Joseph Lepley <josephflepley+pro@gmail.com>
2024-11-17 13:39:29 -0500

    Refactor cell content display logic

--- src/components/Grid/Cell.tsx
+++ src/components/Grid/Cell.tsx
@@ -1,7 +1,8 @@
-import { MouseEvent, useCallback } from "react";
-import { useCellContext } from "../GameManager";
-import CellContent from "./CellContent";
-import CellCover from "./CellCover";
+import { MouseEvent, useCallback, useMemo } from "react";
+import { useCellContext, useGamePhase } from "../GameManager";
+import clsx from "clsx";
+import { GAME_READY } from "../../config/game-phases";
+import MineVector from '../../assets/mine.svg?react';
 
 interface CellProps {
   rowIndex: number
@@ -10,6 +11,7 @@ interface CellProps {
 
 export default function Cell({ rowIndex, columnIndex }: CellProps) {
   const { isRevealed, hasMine, neighboringMineCount, cellFlag, handleCellClick } = useCellContext(rowIndex, columnIndex);
+  const gamePhase = useGamePhase();
 
   const handleClick = useCallback(
     () => {
@@ -25,11 +27,34 @@ export default function Cell({ rowIndex, columnIndex }: CellProps) {
     },
     [handleCellClick],
   );
+
+  const cellContent = useMemo(
+    () => {
+      if (gamePhase === GAME_READY) {
+        return null;
+      }
+
+      if (isRevealed) {
+        if (hasMine) {
+          return (<MineVector />)
+        }
+
+        return neighboringMineCount || null;
+      }
+
+      if (cellFlag) {
+        return 'F';
+      }
+
+      return null;
+    },
+    [gamePhase, isRevealed, hasMine, cellFlag, neighboringMineCount],
+  );
   
 
   return (
     <span className="cell" tabIndex={0} onClick={handleClick} onContextMenu={handleContextMenu}>
-      {isRevealed ? (<CellContent hasMine={hasMine} neighboringMineCount={neighboringMineCount} />) : (<CellCover cellFlag={cellFlag} />)}
+      <span className={clsx('cell-inner', isRevealed ? 'revealed' : 'covered')}>{cellContent}</span>
     </span>
   );
 }
